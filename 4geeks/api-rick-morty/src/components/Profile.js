9b2d60e63252ba7c237b9f9af19302246c9dac67
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2022-07-08 00:25:22 +0000

    useNavigate y useParams :zap:

--- 4geeks/api-rick-morty/src/components/Profile.js
+++ 4geeks/api-rick-morty/src/components/Profile.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
 
 export const Profile = () => {
+
+  let { id } = useParams()
+
+  const [ profile, setProfile ] = useState()
+
+  const navigate = useNavigate()
+
+  const handlerPrev = () => navigate(`/character/${ parseInt(profile.id-1) }`, {replace: true})
+
+  const handlerNext = () => navigate(`/character/${ parseInt(profile.id+1) }`, {replace: true})
+
+  useEffect(()=>{
+    
+    fetch(`https://rickandmortyapi.com/api/character/${id}`)
+      .then(data => data.json())
+      .then(response => setProfile(response))
+      .catch(err => console.log(err))
+
+  },[id])
+
   return (
-    <div>Profile</div>
+    <div>
+      { (profile) ? (<>
+          <h2>{profile.name}</h2>
+          <h3>{profile.species}</h3>
+          <img src={profile.image} alt={profile.name}/>
+          <button onClick={handlerPrev}>prev</button>
+          <button onClick={handlerNext}>next</button>
+          <button onClick={() => navigate('/', {replace: true})}>home</button>
+        </>) 
+        : <h2>...Loading</h2> }
+    </div>
   )
 }
